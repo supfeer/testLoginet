7f8c6fe161bb8a73f6b82af9124a4478b001712d
SUPFEER <supfeer@gmail.com>
2015-09-18 13:07:38 +0300

    Rejected commit: Default

--- test/java/ru/loginet/test/ojects/Dater.java
+++ test/java/ru/loginet/test/ojects/Dater.java
@@ -1,10 +1,41 @@
 package ru.loginet.test.ojects;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Created by supfe_000 on 18.09.2015.
  */
 public class Dater {
 
+    public static String getNow(){
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d.MM.YYYY");
+        return dateTimeFormatter.format(localDate);
+    }
+    public static String getTomorrow(){
+        LocalDate localDate = LocalDate.now();
+        localDate = localDate.plusDays(1);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d.MM.YYYY");
+        return dateTimeFormatter.format(localDate);
+    }
 
-
+    public static String get5daysAgo(){
+        LocalDate localDate = LocalDate.now();
+        localDate = localDate.minusDays(5);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d.MM.YYYY");
+        return dateTimeFormatter.format(localDate);
+    }
+    public static String getNowMinus(int days){
+        LocalDate localDate = LocalDate.now();
+        localDate = localDate.minusDays(days);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d.MM.YYYY");
+        return dateTimeFormatter.format(localDate);
+    }
+    public static String getNowPlus(int days){
+        LocalDate localDate = LocalDate.now();
+        localDate = localDate.plusDays(days);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d.MM.YYYY");
+        return dateTimeFormatter.format(localDate);
+    }
 }
